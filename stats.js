f3bea1fcef96482c0ad2f5fa84d395a770239c85
Tim L <neschampion@gmail.com>
2013-04-06 18:40:55 -0500

    Stats.js update
    Still errors, changed to look for a list of outfit character ids for SNH
    for testing purposes.

--- stats.js
+++ stats.js
@@ -1,7 +1,14 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://census.soe.com/get/ps2-beta/character/5428010618035740161?c:resolve=item_list&c:show=stats.play_time.weapon,stats.kills.weapon", false)
+xhr.open("GET", "https://census.soe.com/s:bringinthestats/get/ps2-beta/outfit?alias=snh&c:resolve=member", false);
 xhr.send();
 
-var JSON = JSON.parse(xhr.responseXML)
+console.log(xhr.status);
+
+var JSON = JSON.parse(xhr.responseXML);
+
+//xhr.open("GET", "http://census.soe.com/s:bringinthestats/get/ps2-beta/character/5428010618035740161?c:resolve=item_list&c:show=stats.play_time.weapon,stats.kills.weapon", false);
+//xhr.send();
+
 console.log(JSON);
+​
